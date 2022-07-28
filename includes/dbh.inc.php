f337490f24f8ee127faa8f5c8520cb836a8d6bac
Shawn Xue <shawn.xyp@gmail.com>
2022-07-28 14:19:34 +1000

    Update database connection method

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,10 +1,14 @@
 <?php
-$serverName = "us-cdbr-east-06.cleardb.net";
-$dbUsername = "be03ba24e8d9a2";
-$dbPassword = "38b94132";
-$dbName = "shimmer-heroku";
-
-$conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
